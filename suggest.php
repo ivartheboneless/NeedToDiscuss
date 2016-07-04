06e6950e8012f2e561b640ea721bf952486384bf
Muhammad Ammar Aamir <ammar2411@gmail.com>
2016-07-05 03:38:37 +0500

    Added suggestion form

--- suggest.php
+++ suggest.php
@@ -1,13 +1,33 @@
 <?php
 
-  $pageTitle = "Suggest a Media Item";
+  $pageTitle = "Suggest another topic for discussion.";
   $section = "suggest";
   include("includes/header.php");
 
 ?>
 
 <div class="section page">
-  <h1> <?php echo $pageTitle ?> </h1>
+  <div class="wrapper">
+    <h1> <?php echo $pageTitle ?> </h1>
+      <p>Can&rsquo;t find your favorite TV Show/Movie? Please let us know by completing the form below. </p>
+      <form method="post">
+        <table>
+          <tr>
+            <th><label for="name">Name</label></th>
+            <td><input type="text" id="name" name="name"/></td>
+          </tr>
+          <tr>
+            <th><label for="email">Email</label></th>
+            <td><input type="text" id="email" name="email"/></td>
+          </tr>
+          <tr>
+            <th><label for="details">Message</label></th>
+            <td><textarea name="details" id="details"></textarea></td>
+          </tr>
+        </table>
+        <input type="submit" value="Send" />
+      </form>
+  </div>
 </div>
 
 <?php include("includes/footer.php"); ?>
